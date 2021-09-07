9f9a1970412576acac6637891cecd7f03b1ec61b
Jacob Dobbelaere <19271@my4county.net>
2021-09-07 09:16:35 -0400

    Added Java Project Files

---
+++ src/javalibs/JavaLibs.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaLibs;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jdobbelaere
+ */
+public class JavaLibs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String firstName = "null";
+        
+        
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Please enter your first name: ");
+        firstName = sc.nextLine();
+        System.out.println("Thank you, ");
+        System.out.println("My name is Yoshikage Kira. I'm 33 years old. ");
+        System.out.println("My house is in the northeast section of Morioh, where all the villas are, and I am not married. ");
+        System.out.println("I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. ");
+        System.out.println("I don't smoke, but I occasionally drink. ");
+        System.out.println("I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. ");
+        System.out.println("After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, ");
+        System.out.println("I usually have no problems sleeping until morning. Just like a baby, ");
+        System.out.println("I wake up without any fatigue or stress in the morning. ");
+        System.out.println("I was told there were no issues at my last check-up. ");
+        System.out.println("I'm trying to explain that I'm a person who wishes to live a very quiet life. ");
+        System.out.println("I take care not to trouble myself with any enemies, like winning and losing, ");
+        System.out.println("that would cause me to lose sleep at night. ");
+        System.out.println("That is how I deal with society, and I know that is what brings me happiness. ");
+        System.out.println("Although, if I were to fight I wouldn't lose to anyone.");
+        
+    }
+    
+}
